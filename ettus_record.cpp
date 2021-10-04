01c099696192f7e30af71d7075f132396fd4978d
Grabt234 <gnorrie12@gmail.com>
2021-10-04 10:47:29 +0200

    added in includes

---
+++ ettus_record.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <complex>
+#include <thread>
+#include <fstream>
+#include <csignal>
+#include <chrono>
+#include <ctime>
+
+#include <boost/program_options.hpp>
+#include <boost/format.hpp>
+
+#include <uhd/usrp/multi_usrp.hpp>
+#include <uhd/exception.hpp>
+#include <uhd/types/tune_request.hpp>
+#include <uhd/types/device_addr.hpp>
+#include <uhd/utils/thread.hpp>
+
+
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World" << endl;
+}
